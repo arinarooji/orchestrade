5d1e234afff69036c45d099a18564c25e2e5e7ba
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-03 12:52:51 -0500

    Model/Schema initial setup, empty seeds file created

---
+++ models/users.js
@@ -0,0 +1,18 @@
+//User Accounts Schema
+//...
+
+//Dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Schema
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  date: { type: String, required: true },
+  url: { type: String, required: true }
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+//Export
+module.exports = Article;
---
+++ scripts/seedsDB.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+mongoose.Promise = global.Promise;
+
+
+//Sample data here...
