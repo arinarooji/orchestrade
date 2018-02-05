35d07d36fa1414f58389696eefb934b218a459d7
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-05 18:25:05 -0500

    GET all route

--- scripts/seedsDB.js
+++ scripts/seedsDB.js
@@ -1,6 +1,15 @@
-const db = require("../models");
 const mongoose = require("mongoose");
+const db = require("../models");
 mongoose.Promise = global.Promise;
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useMongoClient: true
+  }
+);
 
 //Sample data here...
 const testCrowd = [
