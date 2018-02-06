f14cf8e9ac233b322aaa95f54ba600cd59387c61
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-05 19:43:49 -0500

    Inventory findAll route

--- routes/api/inventory.js
+++ routes/api/inventory.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const usersController = require("../../controllers/inventoryController.js");
+const inventoryController = require("../../controllers/inventoryController.js");
 
 // Matches with "/api/inventory"
 router.route("/")
   .get(inventoryController.findAll)
 
-  
+
 module.exports = router;
