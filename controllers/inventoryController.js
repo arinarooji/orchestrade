847bb2e6440ed96c9a8354a15dc28a1ce208d26b
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-05 19:04:38 -0500

    Instrument findAll route

---
+++ controllers/inventoryController.js
@@ -0,0 +1,33 @@
+//Require the Schemas in models directory
+const db = require("../models");
+
+// Defining methods for the usersController (exported to routes)
+module.exports = {
+  findAll: function(req, res) {
+    db.Inventory
+      .find(req.query)
+      .sort({ instrumentName: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Inventory
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Inventory
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Inventory
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+  //Delete
+  //...
+};
