97d4dad44bf1364e2fa4542a45597cef5f9fd59c
Renato <zamudiorenato@gmail.com>
2018-02-07 17:30:31 -0500

    login section of login page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,18 +4,31 @@ import
     Row, 
     Col,
     Button, 
-    Input
+    Input,
+    Label
 } from 'reactstrap';
 
 const Login = () => {
 return (
 <div>
-    <Container classType="fluid">
+    <Container fluid className="text-white bg-dark py-3">
         <Row>
-            <Col lg="6">
-                    Hello World
+            <Col xs="1" lg="1">
             </Col>
-            <Col lg="6">
+            <Col xs="12" lg="5">
+                <h1 className="display-5">Orchestrade</h1>
+            </Col>
+            <Col xs="5" lg="2">
+                <Label >Email</Label>
+                <Input placeholder="name@school.edu" />
+            </Col>
+            <Col xs="5" lg="2">
+                <Label>Password</Label>
+                <Input />
+            </Col>
+            <Col lg="2" className="mt-2">
+                <br />
+                <Button>Login</Button>
             </Col>
         </Row>
     </Container>
