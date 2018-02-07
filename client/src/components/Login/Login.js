e96829cd72ad6f0fd0094ec3d1e28042425e737f
Renato <zamudiorenato@gmail.com>
2018-02-06 21:40:27 -0500

    login component first commit

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import 
+{ Container, 
+    Row, 
+    Col,
+    Button, 
+    Input
+} from 'reactstrap';
+
+const Login = () => {
+return (
+<div>
+    <Container classType="fluid">
+        <Row>
+            <Col lg="6">
+                    Hello World
+            </Col>
+            <Col lg="6">
+            </Col>
+        </Row>
+    </Container>
+</div>
+)
+}
+
+export default Login
