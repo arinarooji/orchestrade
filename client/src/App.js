f7c81eea8a335cfc644cc14399552f18396168f5
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-08 19:48:30 -0500

    restructure, components->pages->App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login'
-import Signup from './components/Signup'
+import LoginPage from './pages/LoginPage'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login/>
-        <Signup/>
+        <LoginPage/>
       </div>
     );
   }
