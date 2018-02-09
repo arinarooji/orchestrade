ec43bac27d63bcaded54417383f6d305c1f59cc4
CrisPortilla <portillacris@gmail.com>
2018-02-08 19:49:23 -0500

    draft

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import Signup from './components/Signup'
+import Navi from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navi/>
         <Login/>
         <Signup/>
       </div>
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+  export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  } 
+  render() {
+  return (
+    <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/"></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">About</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Marketplace
+                </DropdownToggle>
+                <DropdownMenu >
+                  <DropdownItem>
+                    Traders
+                  </DropdownItem>
+                  <DropdownItem>
+                    Tradees
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+  }
+
+  //export default Navi
