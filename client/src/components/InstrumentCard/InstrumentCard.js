74a092fe514ab6c88a3ca08f00582be3f8c516bc
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-17 10:55:26 -0500

    Custom button text values

--- client/src/components/InstrumentCard/InstrumentCard.js
+++ client/src/components/InstrumentCard/InstrumentCard.js
@@ -25,7 +25,7 @@ const InstrumentCard = props => {
           </CardTitle>
           <CardSubtitle>Hosted By: {props.school}</CardSubtitle>
           <CardText>Some quick details about the {props.instrument}.</CardText>
-          <Button>Request</Button>
+          <Button>{props.action}</Button>
         </CardBody>
       </Card>
     </div>
