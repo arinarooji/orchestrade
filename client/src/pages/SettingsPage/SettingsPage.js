cfc69a3464df2e1eec6b611cffe45f9f54946307
adamtre9115 <treadam9115@gmail.com>
2018-02-20 19:58:21 -0500

    update settings page

--- client/src/pages/SettingsPage/SettingsPage.js
+++ client/src/pages/SettingsPage/SettingsPage.js
@@ -1,34 +1,55 @@
 import React, { Component } from "react";
 import Navi from "../../components/Navi";
 import Footer from "../../components/Footer";
-import SettingsContainer from "../../components/SettingsContainer";
 import "./SettingsPage.css";
+import PrivacyPolicy from "../../components/PrivacyPolicy";
+import AccountDetail from "../../components/Account";
+import SideNavi from "../../components/SideNavi";
+import SettingsMobile from "../../components/SettingsMobile";
+import DefaultSettings from "../../components/DefaultSettings";
 
 class Settings extends Component {
   state = {
     page: ""
   };
 
-  // this funcion is being passed as a prop to the settings container
-  // it sets the state equal the specific link that is clicked
+  // this funcion captures the data-value of the link clicked and sets it to the state
   clickedPage = event => {
     const linkClicked = event.currentTarget.dataset.value;
-    this.setState({page: linkClicked}, function(){
-      const something = this.state.page;
-      this.pageChange(something);
-    })
-  }
+    this.setState({ page: linkClicked }, function() {
+      const whichPage = this.state.page;
+    });
+  };
 
-  pageChange = function(whichState){
-    return whichState;
-  }
+  // determines which page should be rendered based off of the current state
+  pageChange = () => {
+    const whichState = this.state.page;
+    if (whichState === "account") {
+      return <AccountDetail />;
+    } else if (whichState === "privacy") {
+      return <PrivacyPolicy />;
+    } else {
+      return <DefaultSettings/>
+    }
+  };
   render() {
-    const thisPage = this.state.page;
     return (
       <div>
         <Navi />
         <div className="addSpace">
-          <SettingsContainer aLinkClicked={this.clickedPage} yourValue={this.state.page} pageToRender={thisPage}/>
+        <div id="mobile">
+          <SettingsMobile aLinkClicked={this.clickedPage}
+                  yourValue={this.state.page}/>
+          </div>
+          <div className="row">
+            <div className="sideMenu col-md-2">
+              <SideNavi
+                aLinkClicked={this.clickedPage}
+                yourValue={this.state.page}
+              />
+            </div>
+            <div id="renderedPage" className="col-md-10">{this.pageChange()}</div>
+          </div>
         </div>
         <Footer />
       </div>
