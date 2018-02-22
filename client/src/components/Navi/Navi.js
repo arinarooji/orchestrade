90cc335d65ee8108a92f7ae01eba4cd0d2467b54
adamtre9115 <treadam9115@gmail.com>
2018-02-22 13:46:51 -0500

    further update to styling using App.scss/App.css (in progress)

--- client/src/components/Navi/Navi.js
+++ client/src/components/Navi/Navi.js
@@ -48,17 +48,17 @@ export default class Navi extends Component {
                   </DropdownToggle>
                   <DropdownMenu>
                     <DropdownItem>
-                      <NavLink href="/add">Add Instruments</NavLink>
+                      <NavLink className="dropdown" href="/add">Add Instruments</NavLink>
                     </DropdownItem>
                     <DropdownItem>
-                      <NavLink href="/manage">Manage Instruments</NavLink>
+                      <NavLink className="dropdown" href="/manage">Manage Instruments</NavLink>
                     </DropdownItem>
                     <DropdownItem divider />
                     <DropdownItem>
-                      <NavLink href="/settings">Settings</NavLink>
+                      <NavLink className="dropdown" href="/settings">Settings</NavLink>
                     </DropdownItem>
                     <DropdownItem>
-                      <NavLink href="/#">Log Out</NavLink>
+                      <NavLink className="dropdown" href="/#">Log Out</NavLink>
                     </DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
