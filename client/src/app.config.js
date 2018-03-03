e85b21b6caeb79f68b2f8108aaf741a0888d4922
adamtre9115 <treadam9115@gmail.com>
2018-03-02 21:30:02 -0500

    rebuild

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,6 @@
 export default {
   url: 'https://dev-298287.oktapreview.com',
   issuer: 'https://dev-298287.oktapreview.com/oauth2/default',
-  redirect_uri: 'https://orchestradeapp.herokuapp.com' + '/implicit/callback',
+  redirect_uri: window.location.origin + '/implicit/callback',
   client_id: '0oae5g5yfkWATvlnK0h7'
   }
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -26,7 +26,7 @@ export default withAuth(class LoginPage extends Component {
   render() {
     if (this.state.authenticated === null) return null;
     return this.state.authenticated ?
-      <Redirect to={{ pathname: '/home' }} /> :
+      <Redirect to={{ pathname: '/' }} /> :
       <div>
       <Login baseUrl={this.props.baseUrl} />;
       <Footer />
