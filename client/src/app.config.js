c5ad0a100ef92a5890cecf976c086fee6f08da23
adamtre9115 <treadam9115@gmail.com>
2018-03-02 21:40:42 -0500

    rebuild

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,6 @@
 export default {
   url: 'https://dev-298287.oktapreview.com',
   issuer: 'https://dev-298287.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
+  redirect_uri: "https://orchestradeapp.herokuapp.com/",
   client_id: '0oae5g5yfkWATvlnK0h7'
   }
